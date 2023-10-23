0d691c386c0c4b36b1da0c893ba5a8b57d7df246
gpbsumido <gpbsumido@gmail.com>
2023-10-23 15:03:10 -0600

    init

---
+++ pages/index.js
@@ -0,0 +1,69 @@
+import styles from '../styles/Home.module.css';
+import { useState } from "react";
+
+import Helika from "helika-sdk";
+import { BaseURLOptions } from 'helika-sdk';
+
+export default function Home() {
+
+  const [sdk,setSdk] = useState(undefined);
+  const [apiKey,setApiKey] = useState('');
+
+  async function initiateSdk(){
+    if (!apiKey || apiKey.trim()?.length < 10) {
+      console.error('Must have valid API Key');
+      return;
+    }
+    const helikaSDK = new Helika.EVENTS({
+      apiKey: apiKey,
+      baseUrlOption: BaseURLOptions.EVENTS_DEV
+    });
+    setSdk(helikaSDK);
+  }
+
+  async function sendEvent(){
+
+    if (!sdk) {
+      console.error('You must initiate the sdk first');
+      return;
+    }
+
+    let events = [{
+      game_id: 'SDK Example Project',
+      event_type: 'Test',
+      event: {
+        id: 1,
+        info: 'From Example Project, send event button'
+      }
+    }];
+
+    sdk.createEvent('321',events);
+  }
+
+  return (
+    <div className={styles.container}>
+      <div>
+        API Key:
+      </div>
+      <input
+        type='text'
+        value={apiKey}
+        onChange={(e)=>{
+          setApiKey(e.target.value);
+        }}
+      />
+      <button
+        onClick={initiateSdk}
+        style={{margin:'1em 0 0 0'}}
+      >
+        Inititate SDK
+      </button>
+      <button
+        onClick={sendEvent}
+        style={{margin:'1em 0 0 0'}}
+      >
+        Send Event
+      </button>
+    </div>
+  );
+}
